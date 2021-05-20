dd835dca16409578f2ee104f57096a0ce5e5ab72
oloj-hub <igorkalinic@mail.ru>
2021-05-20 18:06:39 +0300

    remove .ipynb_checkpoints

--- final_work/.ipynb_checkpoints/gnuplot-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-set terminal png
-set output "RDF.png"
-plot for[plotname in "2.0 2.5 3.0 3.5 4.0"] "~/study/final_work/lj/diam_".plotname."/rdf.txt" using 2:3 w lp title plotname
-set output "ENG.png"
-plot for[plotname in "2.0 2.5 3.0 3.5 4.0"] "~/study/final_work/lj/diam_".plotname."/Eng.txt" using 1:2 w lp title plotname
-set output "Temp.png"
-plot 'Temp.txt' w lp title 'avr_temp'
